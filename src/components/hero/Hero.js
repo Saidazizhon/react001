25f1d1913419e5750172496cef3b0647c37aa89a
Saidazizhon <asrarovs26@gmail.com>
2024-07-30 14:36:33 +0500

    hali tugamadi

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./hero.css"
+
+
+function Hero(){
+    return (
+        <div>
+            <section className="container">
+                
+            </section>
+        </div>
+    )
+}
+
+
+export default Hero
